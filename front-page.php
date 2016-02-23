d11ae50d78ceb75c5011b97318335c59e77a3c08
Curious <baidu211@vip.qq.com>
2016-02-23 23:06:22 +0800

    modify
    modify

--- front-page.php
+++ front-page.php
@@ -1,7 +1,252 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2016/2/23
- * Time: 16:52
- */ 
+get_header();
+?>
+<!--main-->
+<div class="main">
+<div class="container">
+<div class="row">
+<div class="col-md-9 col-sm-12">
+    <!--one col-->
+    <div class="row">
+        <div class="col-md-6 col-sm-6">
+            <div class="panel">
+                <div class="panel-img">
+                    <a href="###"><img class="img-responsive" src="images/them/wordpress-church-themes.jpg"></a>
+                </div>
+                <div class="panel-content">
+                    <div class="panel-title">
+                        <h2><a href="#" title="2016年15个关于教堂报道和布道的响应式主题">2016年15个关于教堂报道和布道的响应式主题</a></h2>
+                    </div>
+                    <ul>
+                        <li><i class="fa fa-clock-o fa-fw"></i> 2015-12-13</li>
+                        <li><i class="fa fa-user fa-fw" title="二批"></i> 二批</li>
+                        <li><i class="fa fa-folder-o fa-fw" ></i> 免费主题</li>
+                        <li><i class="fa fa-comment-o fa-fw"></i> 213213 评论</li>
+                    </ul>
+                    <div class="panel-desc">
+                        <p>
+                            WordPress主题设计和开发人员为教会、宗教和慈善组织,教区,非营利组织、宗教团体、部门和其他类似的网站
+                        </p>
+                                            <span>
+                                                <a href="#" class="button  button-pri button-small"><i class="fa fa-eye fa-fw"></i> 查看全部</a>
+                                            </span>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6">
+            <div class="panel">
+                <div class="panel-img">
+                    <a href="###" title="church-wordpress-themes"><img class="img-responsive" src="images/them/church-wordpress-themes.jpg"></a>
+                </div>
+                <div class="panel-content">
+                    <div class="panel-title">
+                        <h2><a href="#" title="2016年15个关于教堂报道和布道的响应式主题">2016年15个关于教堂报道和布道的响应式主题</a></h2>
+                    </div>
+                    <ul>
+                        <li><i class="fa fa-clock-o fa-fw"></i> 2015-12-13</li>
+                        <li><i class="fa fa-user fa-fw" title="二批"></i> 二批</li>
+                        <li><i class="fa fa-folder-o fa-fw" ></i> 免费主题</li>
+                        <li><i class="fa fa-comment-o fa-fw"></i> 213 评论</li>
+                    </ul>
+                    <div class="panel-desc">
+                        <p>
+                            WordPress主题设计和开发人员为教会、宗教和慈善组织,教区,非营利组织、宗教团体、部门和其他类似的网站
+                        </p>
+                                             <span>
+                                                <a href="#" class="button  button-pri button-small"><i class="fa fa-eye fa-fw"></i> 查看全部</a>
+                                            </span>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    <!-- two col-->
+    <div class="row">
+        <div class="col-md-6 col-sm-6">
+            <div class="panel">
+                <div class="panel-img">
+                    <a href="###"><img class="img-responsive" src="images/them/wordpress-church-themes.jpg"></a>
+                </div>
+                <div class="panel-content">
+                    <div class="panel-title">
+                        <h2><a href="#" title="2016年15个关于教堂报道和布道的响应式主题">2016年15个关于教堂报道和布道的响应式主题</a></h2>
+                    </div>
+                    <ul>
+                        <li><i class="fa fa-clock-o fa-fw"></i> 2015-12-13</li>
+                        <li><i class="fa fa-user fa-fw" title="二批"></i> 二批</li>
+                        <li><i class="fa fa-folder-o fa-fw" ></i> 免费主题</li>
+                        <li><i class="fa fa-comment-o fa-fw"></i> 213213 评论</li>
+                    </ul>
+                    <div class="panel-desc">
+                        <p>
+                            WordPress主题设计和开发人员为教会、宗教和慈善组织,教区,非营利组织、宗教团体、部门和其他类似的网站
+                        </p>
+                                             <span>
+                                                <a href="#" class="button  button-pri button-small"><i class="fa fa-eye fa-fw"></i> 查看全部</a>
+                                            </span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6">
+            <div class="panel">
+                <div class="panel-img">
+                    <a href="###" title="church-wordpress-themes"><img class="img-responsive" src="images/them/church-wordpress-themes.jpg"></a>
+                </div>
+                <div class="panel-content">
+                    <div class="panel-title">
+                        <h2><a href="#" title="2016年15个关于教堂报道和布道的响应式主题">2016年15个关于教堂报道和布道的响应式主题</a></h2>
+                    </div>
+                    <ul>
+                        <li><i class="fa fa-clock-o fa-fw"></i> 2015-12-13</li>
+                        <li><i class="fa fa-user fa-fw" title="二批"></i> 二批</li>
+                        <li><i class="fa fa-folder-o fa-fw" ></i> 免费主题</li>
+                        <li><i class="fa fa-comment-o fa-fw"></i> 213 评论</li>
+                    </ul>
+                    <div class="panel-desc">
+                        <p>
+                            WordPress主题设计和开发人员为教会、宗教和慈善组织,教区,非营利组织、宗教团体、部门和其他类似的网站...
+                        </p>
+                                             <span>
+                                                <a href="#" class="button  button-pri button-small"><i class="fa fa-eye fa-fw"></i> 查看全部</a>
+                                            </span>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="col-md-3 col-sm-12">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="follow">
+                <p>关注我们</p>
+                <ul>
+                    <li><a href="#" class="weixin">
+                            <i class="fa fa-weixin fa-2x"></i>
+                        </a></li>
+                    <li><a href="#" class="weibo">
+                            <i class="fa fa-weibo fa-2x"></i>
+                        </a></li>
+                    <li><a href="#" class="rss">
+                            <i class="fa fa-rss fa-2x"></i>
+                        </a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <!--编辑推荐-->
+            <div class="editor_recommend">
+                <p>编辑推荐</p>
+                <ul>
+                    <li>
+                        <a href="#"><img class="img-responsive " src="images/ad/eighttheme-on-colorlib-1.png"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img class="img-responsive" src="images/ad/theblogstarter-on-colorlib.png"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img class="img-responsive" src="images/ad/themeisle-colorlib.png"></a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <!--我们的主题-->
+            <div class="our_theme">
+                <p>免费主题分类</p>
+                <ul>
+                    <?php
+                    $cat = get_category_by_slug('wpfree');
+                    wp_list_categories(array(
+                        'title_li'=>0,
+                        'hide_empty' => 0,
+                        'child_of' => $cat ->term_id,
+                    ));?>
+                    <?php get_terms('wpfree');?>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="download_theme">
+                <p>下载量排行榜</p>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="images/recomm/a2hosting-review-65x65.jpg" class="  img-responsive">
+                        </a>
+                        <a href="#">
+                            <p>2015年10个html5开发者必备文本编辑器</p>
+                        </a>
+                    </li> <li>
+                        <a href="#">
+                            <img src="images/recomm/colorlib_blog_image_retina_1024-65x65.png" class="img-responsive">
+                        </a>
+                        <a href="#">
+                            <p>2015年10个html5开发者必备文本编辑器</p>
+                        </a>
+                    </li> <li>
+                        <a href="#">
+                            <img src="images/recomm/free-wordpress-video-themes-65x65.jpg" class="img-responsive">
+                        </a>
+                        <a href="#">
+                            <p>2015年10个html5开发者必备文本编辑器</p>
+                        </a>
+                    </li> <li>
+                        <a href="#">
+                            <img src="images/recomm/Northeme-WordPress-Themes-65x65.jpg" class="img-responsive">
+                        </a>
+                        <a href="#">
+                            <p>2015年10个html5开发者必备文本编辑器</p>
+                        </a>
+                    </li> <li>
+                        <a href="#">
+                            <img src="images/recomm/pinbin-theme-instructions-65x65.jpg" class="img-responsive">
+                        </a>
+                        <a href="#">
+                            <p>2015年10个html5开发者必备文本编辑器</p>
+                        </a>
+                    </li> <li>
+                        <a href="#">
+                            <img src="images/recomm/SiteGround-Review-Discount-Codes-Feature-65x65.jpg" class="img-responsive">
+                        </a>
+                        <a href="#">
+                            <p>2015年10个html5开发者必备文本编辑器</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+
+</div>
+
+</div>
+
+
+</div>
+</div>
+<!--main-->
+
+
+
+<?php  get_footer();?>
