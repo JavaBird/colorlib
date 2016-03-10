a1a66b49d7e0cbd89d17026a5245b6bcd2f4c1ca
Curious <baidu211@vip.qq.com>
2016-03-10 17:31:26 +0800

    modify
    modify

--- functions.php
+++ functions.php
@@ -124,6 +124,52 @@ function get_currpost_tags($post){
 }
 
 
+// Register the Cover Image feature for Users profiles
+function bp_default_register_feature() {
+    /**
+     * You can choose to register it for Members and / or Groups by including (or not)
+     * the corresponding components in your feature's settings. In this example, we
+     * chose to register it for both components.
+     */
+    $components = array( 'groups', 'xprofile');
+
+    // Define the feature's settings
+    $cover_image_settings = array(
+        'name'     => 'cover_image', // feature name
+        'settings' => array(
+            'components'   => $components,
+            'width'        => 940,
+            'height'       => 225,
+            'callback'     => 'bp_default_cover_image',
+            'theme_handle' => 'bp-default-main',
+        ),
+    );
+
+
+    // Register the feature for your theme according to the defined settings.
+    bp_set_theme_compat_feature( bp_get_theme_compat_id(), $cover_image_settings );
+}
+add_action( 'bp_after_setup_theme', 'bp_default_register_feature' );
+
+
+
+// Example of function to customize the display of the cover image
+function bp_default_cover_image( $params = array() ) {
+    if ( empty( $params ) ) {
+        return;
+    }
+
+    // The complete css rules are available here: https://gist.github.com/imath/7e936507857db56fa8da#file-bp-default-patch-L34
+    return '
+        /* Cover image */
+        #header-cover-image {
+            display: block;
+            height: ' . $params["height"] . 'px;
+            background-image: url(' . $params['cover_image'] . ');
+        }
+    ';
+}
+
 
 
 
