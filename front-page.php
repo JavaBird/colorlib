af7b5b3a0cb5d0d5163d7795f17c827e83a3d466
Curious <baidu211@vip.qq.com>
2016-03-13 23:22:01 +0800

    modify
    modify

--- front-page.php
+++ front-page.php
@@ -36,10 +36,10 @@ get_header();
                 <p>编辑推荐</p>
                 <ul>
                     <li>
-                        <a href="#"><img class="img-responsive " src="images/ad/eighttheme-on-colorlib-1.png"></a>
+                        <a href="#"><img class="img-responsive " src="http://localhost/wordpress/wp-content/uploads/2016/03/theblogstarter-on-colorlib.png"></a>
                     </li>
                     <li>
-                        <a href="#"><img class="img-responsive" src="images/ad/theblogstarter-on-colorlib.png"></a>
+                        <a href="#"><img class="img-responsive" src="http://localhost/wordpress/wp-content/uploads/2016/03/themeisle-colorlib.png"></a>
                     </li>
                     <li>
                         <a href="#"><img class="img-responsive" src="images/ad/themeisle-colorlib.png"></a>
